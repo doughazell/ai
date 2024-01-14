c6ca588e95feb8e6b1aaceaac4550ea4679ca0d0
Doug Hazell <doughazell@gmail.com>
2024-01-14 23:57:11 +0000

    Code to decode the BART input for a paragraph of text to be summarized.

---
+++ bart/code-translator.py
@@ -0,0 +1,90 @@
+# 14/1/24 DH:
+from transformers.utils import is_torch_available
+
+if is_torch_available():
+  from transformers.models.auto.modeling_auto import MODEL_FOR_SEQ_TO_SEQ_CAUSAL_LM_MAPPING, AutoModelForSeq2SeqLM
+
+from transformers import AutoTokenizer, BartForConditionalGeneration
+import torch
+
+# 14/1/24 DH:
+print("--------------------------------")
+print("Using Transformers 'pipeline'")
+print("--------------------------------")
+
+# 14/1/24 DH: 'transformers/pipelines/base.py' :
+"""
+class Pipeline(_ScikitCompat):
+
+  Base class implementing pipelined operations. Pipeline workflow is defined as a sequence of the following 
+  operations: 
+    Input -> Tokenization -> Model Inference -> Post-Processing (task dependent) -> Output
+
+  @abstractmethod
+  def preprocess(self, input_: Any, **preprocess_parameters: Dict) -> Dict[str, GenericTensor]:
+    Preprocess will take the `input_` of a specific pipeline and return a dictionary of everything necessary for
+    `_forward` to run properly. It should contain at least one tensor, but might have arbitrary other items.
+    
+"""
+# 14/1/24 DH: 'transformers/pipelines/text2text_generation.py' :
+"""
+class Text2TextGenerationPipeline(Pipeline):
+
+  # At end of 'Tokenization' step
+  def preprocess(self, inputs, truncation=TruncationStrategy.DO_NOT_TRUNCATE, **kwargs):
+    inputs = self._parse_and_tokenize(inputs, truncation=truncation, **kwargs)
+    return inputs
+
+  def _forward(self, model_inputs, **generate_kwargs):
+    ...
+    output_ids = self.model.generate(**model_inputs, **generate_kwargs)
+
+"""
+# 14/1/24 DH: 'inputs' printed from 'pipeline.py' are 'BART-double-coded' values
+
+model = BartForConditionalGeneration.from_pretrained("facebook/bart-large-cnn")
+tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
+
+intStrList = []
+with open("bart-double-codes.txt") as source :
+  for line in source.readlines():
+    # Remove newline character from each int printout line
+    lineStrip = line.rstrip()
+
+    for item in lineStrip.split(","):
+      # Remove whitespace
+      itemStrip = item.strip()
+      if itemStrip:
+        intStrList.append(itemStrip)
+
+int2DList = [[int(item) for item in intStrList]]
+input_ids = torch.tensor(int2DList)
+
+print()
+print(tokenizer.batch_decode(input_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0])
+print()
+
+summary_ids = model.generate(input_ids, num_beams=2, min_length=0, max_length=130)
+print(tokenizer.batch_decode(summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0])
+
+#MODEL_FOR_SEQ_TO_SEQ_CAUSAL_LM_MAPPING
+#autoSeq2Seq = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn")
+# 14/1/24 DH: "models/bart/modeling_bart.py:class BartForConditionalGeneration(BartPretrainedModel):"
+
+#print("AutoModelForSeq2SeqLM: ",AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn") )
+# OUTPUT: 
+"""
+BartForConditionalGeneration(
+  (model): BartModel(
+    (shared): Embedding(50264, 1024, padding_idx=1)
+    (encoder): BartEncoder(
+    )
+    (decoder): BartDecoder(
+    )
+  )
+  (lm_head): Linear(in_features=1024, out_features=50264, bias=False)
+)
+"""
+
+
+
