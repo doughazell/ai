55bc7994dfa201637d5fa19860dbd03ee7a3eb35
Doug Hazell <doughazell@gmail.com>
2024-03-30 14:21:12 +0000

    Graphing the {start, middle, end} logits from the training run. Also:   # 30/3/24 DH: We've hooked the hook...:)   import huggin_utils   huggin_utils.logLogits(input_ids, start_logits, end_logits, start_loss, end_loss)

--- huggingface/graph-logits.py
+++ huggingface/graph-logits.py
@@ -96,29 +96,97 @@ def displayLogits(recordsDict):
     if "logits" in key:
       print(f"{key}:")
       for logitsKey in recordsDict[key]:
+        # Truncates to 12 per line so need to handcraft the layout
         #logitsVals = recordsDict[key][logitsKey]
+
         logitsVals = ""
+        cnt = 0
         for val in recordsDict[key][logitsKey]:
+          cnt += 1
           logitsVals += f"{val}, "
 
-        print(f"  {logitsKey}: {logitsVals}")
+        print(f"  {logitsKey} ({cnt}): {logitsVals}")
+    elif "input_ids" in key:
+      input_ids_len = len(recordsDict[key])
+      print(f"{key} ({input_ids_len}): {recordsDict[key]}")
     else:
       print(f"{key}: {recordsDict[key]}")
+
+# 30/3/24 DH: There is an A & B order to data with B being consistently different in a proportion of the epochs
+def pruneLogits(recordsDict):
+  print()
+  print("Pruning:")
+  print("-------")
+
+  tokenLen = len(recordsDict['input_ids'])
+
+  for key in recordsDict:
+    if "logits" in key:
+      print(f"{key}:")
+      deleteList = []
+      for epochKey in recordsDict[key]:
+        logitsLen = len(recordsDict[key][epochKey])
+        if logitsLen != tokenLen + 2:
+          print(f"  {epochKey}) logitsLen: {logitsLen}, tokenLen: {tokenLen}")
+          deleteList.append(epochKey)
+
+      for item in deleteList:
+        del recordsDict[key][item]
+  print("-------")
+
+# 30/3/24 DH: The 'start_logits' should correlate with 'end_logits' so only need to check 'start_logits'
+def getEpochList(recordsDict):
+  epochList = []
+  epochs = list(recordsDict['start_logits'].keys())
+  epochsLen  = len(epochs)
+  middleIdx = round(epochsLen / 2)
+  print(f"epochs: {epochs}, middleIdx: {middleIdx}")
+
+  epochList.append(epochs[0])
+  epochList.append(epochs[middleIdx])
+  epochList.append(epochs[-1])
+
+  return epochList
+
+def graphLogitLines(recordsDict, keyVal, xVals):
+  # 30/3/24 DH: Only graph {start, middle, end} logit values
+  epochList = getEpochList(recordsDict)
+  epochListLen = len(epochList) - 1
+  
+  for epoch in epochList:
+    lwVal = (int(epoch) / epochListLen) / 3
+
+    yVals = recordsDict[keyVal][epoch]
+    
+    xValsLen = len(xVals)
+    yValsLen = len(yVals)
+    if xValsLen == yValsLen:
+      
+      if "end" in keyVal:
+        plt.plot(xVals, yVals, label=f"{epoch}", linestyle='dashed', linewidth=lwVal)
+      else:
+        plt.plot(xVals, yVals, label=f"{epoch}", linewidth=lwVal)
+
+    # Shouldn't need this anymore after 'pruneLogits()'
+    else:
+      print(f"  KEY: {epoch}, xValsLen: {xValsLen}, yValsLen: {yValsLen}")
 
 # Taken from: 'ai/huggingface/stop_trainer_utils.py'
 def graphLogits(recordsDict):
   # 4/9/23 DH: Display all graphs simultaneously with 'plt.show(block=False)' (which needs to be cascaded)
   plt.figure()
 
-  plt.title("Token values from different training epochs")
+  plt.title("Logits from different training epochs")
   plt.xlabel("Token ID")
   plt.ylabel("Logit value")
 
   xValNum = recordsDict['input_ids'].shape[0]
   xVals = range(xValNum + 2)
-  yVals = recordsDict['start_logits']['1']
 
-  plt.plot(xVals, yVals, label="Token values")
+  graphLogitLines(recordsDict, "start_logits", xVals)
+  graphLogitLines(recordsDict, "end_logits", xVals)
+  plt.legend(loc="upper left")
+  
   plt.axhline(y=0, color='green', linestyle='dashed', linewidth=0.5)
 
   #plt.draw()
@@ -127,4 +195,5 @@ def graphLogits(recordsDict):
 if __name__ == "__main__":
   recordsDict = collectLogits()
   displayLogits(recordsDict)
+  pruneLogits(recordsDict)
   graphLogits(recordsDict)
--- huggingface/huggin_utils.py
+++ huggingface/huggin_utils.py
@@ -5,6 +5,69 @@
 #
 ##############################################################################
 
+# 30/3/24 DH:
+def niceWorkGoodjob():
+  print()
+  print(f"  niceWorkGoodjob")
+  print()
+
+# 30/3/24 DH:
+inputIdsWritten = False
+epochNum = 0
+
+def logLogits(input_ids, start_logits, end_logits, start_loss, end_loss):
+  global epochNum
+  global inputIdsWritten
+
+  # 28/3/24 DH: Use Python logging rather than 'transformers.utils.logging'
+  #             (Take first entry of overflow batch from Dataset #10 with batch size #8)
+  if start_logits.shape[0] == 2:
+  
+    epochNum += 1
+    
+    import logging
+    sigLogger = logging.getLogger("trainer_signaller")
+
+    inputIdsEnd = (input_ids[0] == 0).nonzero()[0]
+    # Need to add in 'input_ids[0]' on first time using global 'inputIdsWritten'
+    # 30/3/24 DH: However it looks like there is randomisation of the dataset between epochs...!
+    if not inputIdsWritten:
+      inputIdsWritten = True
+      sigLogger.info(f"input_ids[0]: {input_ids[0][0:inputIdsEnd]}")
+
+    endDelta = 10
+    endIdx = inputIdsEnd + endDelta
+
+    postStartList = (start_logits[0][inputIdsEnd:endIdx]).tolist()
+    postStartList = [round(value,2) for value in postStartList]
+    postStartEndList = (start_logits[0][-endDelta:]).tolist()
+    postStartEndList = [round(value,2) for value in postStartEndList]
+    print()
+    print(f"  POST start ({inputIdsEnd}-{endIdx}): {postStartList}")
+    print(f"             (-{endDelta}): {postStartEndList}")
+
+    postEndList = (end_logits[0][inputIdsEnd:endIdx]).tolist()
+    postEndList = [round(value,2) for value in postEndList]
+    postEndEndList = (end_logits[0][-endDelta:]).tolist()
+    postEndEndList = [round(value,2) for value in postEndEndList]
+    print(f"  POST end ({inputIdsEnd}-{endIdx}): {postEndList}")
+    print(f"            (-{endDelta}): {postEndEndList}")
+    print()
+
+    startDelta = 2
+    idxEnd = inputIdsEnd + startDelta
+
+    startLogitsList = (start_logits[0][0:idxEnd]).tolist()
+    startLogitsList = [round(value,2) for value in startLogitsList]
+    sigLogger.info(f"{epochNum}) startLogitsList (+{startDelta}): {startLogitsList}")
+    sigLogger.info(f"  {epochNum}) start_loss: {start_loss}")
+
+    endLogitsList = (end_logits[0][0:idxEnd]).tolist()
+    endLogitsList = [round(value,2) for value in endLogitsList]
+    sigLogger.info(f"{epochNum}) endLogitsList (+{startDelta}): {endLogitsList}")
+    sigLogger.info(f"  {epochNum}) end_loss: {end_loss}")
+
+
 # https://github.com/huggingface/transformers/tree/main/examples/pytorch/question-answering#fine-tuning-bert-on-squad10
 # "You might need to tweak the data processing inside the script if your data is structured differently."
 def stripListLayer(examples):
